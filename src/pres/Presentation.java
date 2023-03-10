6e5e9b5a04da2842167d3a9e54b65e42ac574700
HVDE5 <am.lmarzouqy@outlook.fr>
2023-03-10 22:27:56 +0000

    fc

--- src/pres/Presentation.java
+++ src/pres/Presentation.java
@@ -1,14 +1,25 @@
 package pres;
 
-import dao.DaoImpl;
-import metier.MetierImpl;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+import dao.IDao;
+import metier.IMetier;
 
 public interface Presentation {
-	
 	public static void main(String[] args) {
-		DaoImpl dao = new DaoImpl();
-		MetierImpl metier=new MetierImpl();
-		metier.setDao(dao);
-		System.out.printf("%,.2f °C",metier.calcul());
+		File f =new File("src/config.txt");
+		try (Scanner scanner = new Scanner(f)) {
+			String daoClassname=scanner.next();
+			String metierClassName=scanner.next();
+			Class<?> cdao=Class.forName(daoClassname);
+			IDao dao= (IDao) cdao.getDeclaredConstructor().newInstance();
+			Class<?> cmetier=Class.forName(metierClassName);
+			IMetier metier=(IMetier) cmetier.getDeclaredConstructor().newInstance();
+			Method method=cmetier.getMethod("setDao",IDao.class);
+			method.invoke(metier,dao);
+			System.out.printf("%,.2f °C",metier.calcul());
+			} catch (Exception e) { e.printStackTrace(); }
+
 	}
 }
